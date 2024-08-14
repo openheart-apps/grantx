caf27522ff6683f203300b1bc3b264b0975ab9a8
harshsbhat <harsh121102@gmail.com>
2024-08-14 19:14:04 +0530

    Added login page and signup page

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/auth/signin',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
